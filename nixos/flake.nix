ce4b17ec6d62542cc7d795fec3ad4316d95d0466
KotzaBoss <georgios.kotzampopoulos@tutanota.com>
2024-06-07 15:14:04 +0200

    Small tweak in flake.nix for "let pkgs"

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,7 +8,7 @@
 	outputs = { self, nixpkgs, ... }:
 	let
 		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
+		pkgs = import nixpkgs { inherit system; };
 	in {
 		devShells.x86_64-linux.default = pkgs.mkShell {
 
