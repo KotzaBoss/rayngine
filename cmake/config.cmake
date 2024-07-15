e10d72606c8fd88657749518247030b8d325449f
KotzaBoss <georgios.kotzampopoulos@tutanota.com>
2024-07-15 15:30:50 +0200

    Fix calls to new cconfig()

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -3,7 +3,7 @@ include(utils)
 # See bottom of file for variable postprocessing.
 # tl;dr: The path variables are passed through string(CONFIGURE)
 
-cconfig(RAYNGINE_VERBOSE BOOL OFF "")
+cconfig(RAYNGINE_VERBOSE BOOL OFF DOC "")
 
 if (NOT CMAKE_BUILD_TYPE)
 	message(WARNING "CMAKE_BUILD_TYPE was not set, defaulting to \"Debug\"")
@@ -11,10 +11,10 @@ if (NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "" FORCE)
 	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release")
 endif()
-cconfig(RAYNGINE_BUILD_TYPE INTERNAL ${CMAKE_BUILD_TYPE} "")
+cconfig(RAYNGINE_BUILD_TYPE INTERNAL ${CMAKE_BUILD_TYPE} DOC "")
 
 
 # Resources
-cconfig(RAYNGINE_RESOURCE_DIR PATH "${CMAKE_BINARY_DIR}/res" "")
-cconfig(RAYNGINE_MINI_SPACE_PACK_DIR PATH "${RAYNGINE_RESOURCE_DIR}/Mini_space_pack" "")
+cconfig(RAYNGINE_RESOURCE_DIR PATH "${CMAKE_BINARY_DIR}/res" DOC "")
+cconfig(RAYNGINE_MINI_SPACE_PACK_DIR PATH "${RAYNGINE_RESOURCE_DIR}/Mini_space_pack" DOC "")
 
